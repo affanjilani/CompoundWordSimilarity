5ec49d6e21fe4902ec7401368f7eb850c6857dab
Saphia Petard <saphiapetard@itdepts-MacBook-Air-9.local>
2019-11-29 13:33:49 -0500

    Parameter tuning function added

--- code/model_creation/create_model.py
+++ code/model_creation/create_model.py
@@ -2,6 +2,8 @@
 
 import nltk
 from sklearn.linear_model import Logistic Regression
+from sklearn.model_selection import GridSearchCV
+import numpy as np
 
 
 # input: tuple_list with the following format - [([List of parameter values]1, label1), ([List of parameter values]2, label2), etc...]
@@ -13,3 +15,16 @@ def make_model(tuple_list):
 
 	return classifierLR
 
+def param_tuning(features, labels):
+	
+	classifier = nltk.classify.SklearnClassifier(LogisticRegression())
+	#create hyperparameter search space
+	penalty = ['l1','l2']
+	C = np.logspace(0,4,10)
+	hyperparameters = dict(C=C, penalty=penalty)
+
+	#create + do grid search
+	gridsearch = GridSearchCV(model,hyperparameters,cv=5,verbose=0)
+	best_model = gridsearch.fit(features, labels)
+
+	return best_model.predict(features)
