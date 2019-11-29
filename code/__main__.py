5b571efaf2411722b98f454ee4e6eeda302da892
affanjilani <obiaffan@gmail.com>
2019-11-29 16:36:47 -0500

    Initialized a quick log regression model

--- code/__main__.py
+++ code/__main__.py
@@ -4,9 +4,15 @@ from sklearn.linear_model import LogisticRegression
 
 # Create the model(s)
 
+logReg = LogisticRegression(solver='lbfgs')
+
+
 
 # Split into training and evaluation
 
 # Train the models
+# Assume x_train, y_train, x_test, y_test
+logReg.fit(x_train,y_train)
 
 # Evaluate the models using the validation set
+print(logReg.score(x_test,y_test))
