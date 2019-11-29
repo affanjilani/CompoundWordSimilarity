642680817a617fbeb937e36bb1df15b669c99df8
affanjilani <obiaffan@gmail.com>
2019-11-29 12:47:28 -0500

    Added functions to append and load pretrained word vectors

--- code/doc2vec/word_embeddings.py
+++ code/doc2vec/word_embeddings.py
@@ -26,6 +26,17 @@ def generate_word_2_vec():
 
     return model
 
+# Appends pre-trained vectors to the current vector list
+def add_pretrained_vecs(current_vecs, *args):
+    pass
+
+#Loads pre-trained word2vec vectors into 
+def load_pretrained_vecs(filename):
+    datapath = '../../raw_data/'
+    return gensim.models.KeyedVectors.load_word2vec_format(os.path.join(datapath, filename), binary=True) 
+
+
+
 # Generates the list of documents that get used to train the word2vec model
 def generate_documents(data_file):
     with gzip.open(data_file,'rb') as f:
@@ -37,8 +48,12 @@ if __name__ == '__main__':
     model = gensim.models.Word2Vec.load('./opin/opin_rank_vectors')
     vecs = model.wv
     del model
-    w1 = "asdf"
+    w1 = "butterfly"
     print("Most similar to {0}".format(w1), vecs.most_similar(positive=w1))
+    del vecs
+    
+    #vecs = load_pretrained_vecs('GoogleNews-vectors-negative300.bin')
+    
 
 
 
